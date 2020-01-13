5cb8acacd44c7761f9a0c7f883b51b2ac52e70fe
Juan Jolon <jjolong@miumg.edu.gt>
2020-01-13 15:23:42 -0600

    update api prices

--- routes/api.php
+++ routes/api.php
@@ -10,15 +10,19 @@ Route::get('getSedes','ServiciosRest@getSede');
 // Route::get('categoriess','ServiciosRest@getApi');
 Route::get('getprecio/{id}/{categoria}','ServiciosRest@getPricePrevious');
 Route::get('getprecios/{id}/{categoria}','ServiciosRest@getPriceLast');
-Route::get('price/{id}/{categoria}','ServiciosRest@apiPrice');
+
 
 Route::get('fecha/{sede}/{categoria}', 'ServiciosRest@getPriceLastPrevious');
 Route::get('prueba/{sede}/{categoria}', 'ServiciosRest@getPriceLastPrevious');
 
 
 //Route::get('categories', 'ServiciosRest@collectionDataApi');
+
+//cambio 13 enero
 // Route::get('categories', 'ServiciosRest@VerifyActiveDepartments');
+// Route::get('price/{id}/{categoria}','ServiciosRest@apiPrice');
 Route::get('categories', 'MovilApp@movile_app');
+Route::get('price/{id}/{categoria}','MovilApp@apiPrice_app');
 
 Route::get('VerifyActiveDepartments','ServiciosRest@VerifyActiveDepartments');
 
@@ -26,6 +30,7 @@ Route::get('VerifyActiveDepartments','ServiciosRest@VerifyActiveDepartments');
 
 //******Api Movil
 Route::get('verify','MovilApp@movile_app');
+Route::get('precio/{id}/{categoria}','MovilApp@apiPrice_app');
 
 //***************
 
