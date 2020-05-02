fc524f0fe95bb26bcdabf52feb720a3f74b9636c
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2020-05-01 18:28:41 -0600

    design_asig

--- resources/js/components/vuetify/report/design.vue
+++ resources/js/components/vuetify/report/design.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-container style="height: 500px; ">
+    <el-container style="height: 700px; ">
       <el-aside width="300px">
         <el-card class="box-card">
           <el-row :gutter="10">
@@ -37,12 +37,87 @@
               </el-select>
             </el-col>
           </el-row>
+          <el-row :gutter="10">
+            <el-col :span="10" class="mt-3 mb-2">Tipo:</el-col>
+          </el-row>
+          <el-row :gutter="10">
+            <el-col>
+              <el-select v-model="model_request.model_type_category" placeholder="Categoría">
+                <el-option
+                  v-for="(item, i) in list_response.list_type_category"
+                  v-bind:key="i"
+                  :label="item.name"
+                  :value="item.code" 
+                ></el-option>
+              </el-select>
+            </el-col>
+          </el-row>
+          <el-row :gutter="10">
+            <el-col :span="10" class="mt-3 mb-2">Categoría:</el-col>
+          </el-row>
+          <el-row :gutter="10">
+            <el-col>
+              <el-select v-model="model_request.model_category" placeholder="Categoría">
+                <el-option
+                  v-for="(item, i) in list_response.list_category"
+                  v-bind:key="i"
+                  :label="item.nombre"
+                  :value="item.id_Categoria" 
+                ></el-option>
+              </el-select>
+            </el-col>
+          </el-row>
+          <el-row :gutter="10">
+            <el-col :span="15">
+              <div class="mt-3 ">
+                <span class="mb-3">Fecha Inicial:</span>
+                <el-date-picker
+                  v-model="model_request.model_range_initial"
+                  type="date"
+                  size="large"
+                  clearable
+                  placeholder="Inicio">
+                </el-date-picker>
+              </div>
+            </el-col>
+          </el-row>
+          <el-row :gutter="10">
+            <el-col :span="15">
+              <div class="mt-3 ">
+                <span class="mb-3">Fecha Final:</span>
+                <el-date-picker
+                  v-model="model_request.model_range_final"
+                  type="date"
+                  size="large"
+                  clearable
+                  placeholder="Fin">
+                </el-date-picker>
+              </div>
+            </el-col>
+          </el-row>
+          <el-row >
+            <div class="mt-3">
+              <el-button type="primary">Generar</el-button>
+            </div>
+          </el-row>
         </el-card>
       </el-aside>
+      <el-container>
+        <el-main>
+          <div class="d-flex justify-content-center" v-if="loading_true">
+            <div class="spinner-grow  text-primary" style="width: 4rem; height: 4rem; margin-top: 200px;" role="status">
+              <span class="sr-only">Loading...</span>
+            </div>
+          </div>
+        </el-main>
+      </el-container>
     </el-container>
   </div>
 </template>
 
+<style lang="css">
+
+</style>
 <script>
 export default {
   data() {
@@ -50,19 +125,30 @@ export default {
       url_response: {
         departament: "departamentos",
         municipios: "getMunicipio",
+        type: "findAllVisita",
+        category: "getCategory",
       },
       list_response: {
         list_departament: [],
         list_municipios: [],
+        list_category: [],
+        list_type_category: [],
       },
       model_request: {
         model_departament: "",
         model_municipio: "",
-      }
+        model_category: "",
+        model__type_category: "",
+        model_range_initial:"",
+        model_range_final:"",
+      },
+      loading_true:false
     };
   },
   mounted() {
     this.getDepartament();
+    this.getTypeCategory();
+    this.getCategory();
   },
   methods: {
     getDepartament() {
@@ -79,6 +165,16 @@ export default {
         .then(response => {
           this.list_response.list_municipios = response.data; 
         });
+    },
+    getTypeCategory() {
+      axios.get(this.url_response.type).then(response => {
+        this.list_response.list_type_category = response.data;
+      });
+    },
+    getCategory() {
+      axios.get(this.url_response.category).then(response => {
+        this.list_response.list_category = response.data;
+      });
     }
   }
 };
