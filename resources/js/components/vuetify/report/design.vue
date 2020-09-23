00f5d27c5933791cb374a8e6fc4e09e73c3a6d54
Juan Jose Jolon <jjolong@miumg.edu.gt>
2020-09-23 15:10:59 -0600

    response array

--- resources/js/components/vuetify/report/design.vue
+++ resources/js/components/vuetify/report/design.vue
@@ -434,13 +434,14 @@ export default {
                     db: this.model_request.radio_select_db
                 })
                 .then(response => {
-                    
+                    console.log("Respuesta ", response.data.data);
                     const status = JSON.parse(response.status);
-                    const length = response.data.length; 
+                    const length = response.data.data.length; 
+                    console.log(length);
                     if (status == "200" && length > 0 ) {
-                        console.log(response.data)
-                        this.list_response.list_Local = response.data;
-                        console.log(response.data);
+                        // console.log("datos",response.data.data)
+                        this.list_response.list_Local = response.data.data;
+                        
                         this.loading_true = false;
                     }else{
                         this.message_table.mensajeData =
